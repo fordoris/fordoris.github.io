d42c5a5bc0cbe2a6976a28364ea03c20cf67f377
timlzh <2921349622@qq.com>
2019-11-22 23:55:41 +0800

    Back

--- Our Story of & love_files/functions.js
+++ Our Story of & love_files/functions.js
@@ -43,30 +43,49 @@ function timeElapse(date){
 	var days = Math.floor(seconds / (3600 * 24));
 	seconds = seconds % (3600 * 24);
 	var hours = Math.floor(seconds / 3600);
-	if (-days < 10) {
-		days = "0" + -days;
-	}else{
-		days = -days;
-	}
-	if (-hours < 10) {
-		hours = "0" + -hours;
-	}else{
-		hours = -hours;
+	if (hours < 10) {
+		hours = "0" + hours;
 	}
 	seconds = seconds % 3600;
 	var minutes = Math.floor(seconds / 60);
-	if (-minutes < 10) {
-		minutes = "0" + -minutes;
-	}else{
-		minutes = -minutes;
+	if (minutes < 10) {
+		minutes = "0" + minutes;
 	}
 	seconds = seconds % 60;
-	if (-seconds < 10) {
-		seconds = "0" + -seconds;
-	}else{
-		seconds = -seconds;
+	if (seconds < 10) {
+		seconds = "0" + seconds;
 	}
 	var result = " <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 时 <span class=\"digit\">" + minutes + "</span> 分 <span class=\"digit\">" + seconds + "</span> 秒";
+
+	// var current = Date();
+	// var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+	// var days = Math.floor(seconds / (3600 * 24));
+	// seconds = seconds % (3600 * 24);
+	// var hours = Math.floor(seconds / 3600);
+	// if (-days < 10) {
+	// 	days = "0" + -days;
+	// }else{
+	// 	days = -days;
+	// }
+	// if (-hours < 10) {
+	// 	hours = "0" + -hours;
+	// }else{
+	// 	hours = -hours;
+	// }
+	// seconds = seconds % 3600;
+	// var minutes = Math.floor(seconds / 60);
+	// if (-minutes < 10) {
+	// 	minutes = "0" + -minutes;
+	// }else{
+	// 	minutes = -minutes;
+	// }
+	// seconds = seconds % 60;
+	// if (-seconds < 10) {
+	// 	seconds = "0" + -seconds;
+	// }else{
+	// 	seconds = -seconds;
+	// }
+	// var result = " <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 时 <span class=\"digit\">" + minutes + "</span> 分 <span class=\"digit\">" + seconds + "</span> 秒";
 	//var result = " <span class=\"digit\">" + -days + "</span> 天 <span class=\"digit\">" +-hours + "</span> 时 <span class=\"digit\">" + -minutes + "</span> 分 <span class=\"digit\">" + -seconds + "</span> 秒"; 
 	$("#clock").html(result);
 }
